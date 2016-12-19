5ecc4c0906305e46942c1c0b4a20c595a46b7062
Alex <Alex@192.168.0.103>
2016-12-19 19:58:53 +0800

    fix problem

--- src/main/resources/static/js/searchbar.js
+++ src/main/resources/static/js/searchbar.js
@@ -26,4 +26,19 @@ window.onload=function(){
 			}
 		}
 	}
+	
+	if(document.getElementById("divOneLeft").clientHeight<document.getElementById("divOneRight").clientHeight){
+		document.getElementById("divOneLeft").style.height=document.getElementById("divOneRight").offsetHeight+"px";
+	}
+	else{
+		document.getElementById("divOneRight").style.height=document.getElementById("divOneLeft").offsetHeight+"px";
+	}
+	
+	if(document.getElementById("divTwoLeft").clientHeight<document.getElementById("divTwoRight").clientHeight){
+		document.getElementById("divTwoLeft").style.height=document.getElementById("divTwoRight").offsetHeight+"px";
+	}
+	else{
+		document.getElementById("divTwoRight").style.height=document.getElementById("divTwoLeft").offsetHeight+"px";
+	}
+	
 };
