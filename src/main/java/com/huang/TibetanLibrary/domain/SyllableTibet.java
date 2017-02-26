6768f43c949fab1b8ea460a769d2a6113e75b4eb
Alex <Alex@192.168.0.104>
2017-02-26 23:26:45 +0800

    20170226

--- src/main/java/com/huang/TibetanLibrary/domain/SyllableTibet.java
+++ src/main/java/com/huang/TibetanLibrary/domain/SyllableTibet.java
@@ -16,7 +16,15 @@ public class SyllableTibet {
 	private String nuclensText; 			//韵核  
 	private String codaText; 				//韵尾  
 	private String toneText; 				//声调 
-	private boolean integrated; 				//是否完整的音节（true
+	
+	private String shellWilleText; 			//声韵  
+	private String onsetWilleText; 			//声母 
+	private String finalWilleText; 			//韵母 
+	private String nuclensWilleText; 		//韵核  
+	private String codaWilleText; 			//韵尾  
+	private String toneWilleText; 			//声调 
+	
+	private boolean integrated; 			//是否完整的音节（true
 	
 	public long getID() {
 		return ID;
@@ -95,6 +103,42 @@ public class SyllableTibet {
 	}
 	public void setToneText(String toneText) {
 		this.toneText = toneText;
+	}
+	public String getShellWilleText() {
+		return shellWilleText;
+	}
+	public void setShellWilleText(String shellWilleText) {
+		this.shellWilleText = shellWilleText;
+	}
+	public String getOnsetWilleText() {
+		return onsetWilleText;
+	}
+	public void setOnsetWilleText(String onsetWilleText) {
+		this.onsetWilleText = onsetWilleText;
+	}
+	public String getFinalWilleText() {
+		return finalWilleText;
+	}
+	public void setFinalWilleText(String finalWilleText) {
+		this.finalWilleText = finalWilleText;
+	}
+	public String getNuclensWilleText() {
+		return nuclensWilleText;
+	}
+	public void setNuclensWilleText(String nuclensWilleText) {
+		this.nuclensWilleText = nuclensWilleText;
+	}
+	public String getCodaWilleText() {
+		return codaWilleText;
+	}
+	public void setCodaWilleText(String codaWilleText) {
+		this.codaWilleText = codaWilleText;
+	}
+	public String getToneWilleText() {
+		return toneWilleText;
+	}
+	public void setToneWilleText(String toneWilleText) {
+		this.toneWilleText = toneWilleText;
 	}
 	public boolean getIntegrated() {
 		return integrated;
--- src/main/java/com/huang/TibetanLibrary/util/CentralUtil.java
+++ src/main/java/com/huang/TibetanLibrary/util/CentralUtil.java
@@ -1,6 +1,8 @@
 package com.huang.TibetanLibrary.util;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.huang.TibetanLibrary.domain.LanguagePoint;
 
@@ -14,11 +16,36 @@ public class CentralUtil {
 	}
 	
 	public static void main(String[] args){
-		int i = 0;
-		 for(int tibetanUnm = 3; tibetanUnm < 22; tibetanUnm += 2){
-			 
-			 System.out.println(i++);
-			 System.out.println(tibetanUnm);
-		 }
+//		int i = 0;
+//		 for(int tibetanUnm = 3; tibetanUnm < 22; tibetanUnm += 2){
+//			 
+//			 System.out.println(i++);
+//			 System.out.println(tibetanUnm);
+//		 }
+		String str = "hinm";
+		Pattern p = Pattern.compile("(.*)[aeiou](.*)");
+        Matcher m = p.matcher(str);
+        String matchStr = "";
+		while(m.find()){
+			matchStr = m.group(1);
+		}
+		System.out.println(str.substring(0, matchStr.length()));
+		System.out.println(str.substring(matchStr.length(), matchStr.length()+1));
+		System.out.println(str.substring(matchStr.length()+1, str.length()));
+		System.out.println(matchStr.length());
+        
 	}
+	
+    private static void getStrings() {
+        String str = "rrwerqq84461376qqasfdasdfrrwerqq84461377qqasfdasdaa654645aafrrwerqq84461378qqasfdaa654646aaasdfrrwerqq84461379qqasfdasdfrrwerqq84461376qqasfdasdf";
+        Pattern p = Pattern.compile("qq(.*?)qq");
+        Matcher m = p.matcher(str);
+        ArrayList<String> strs = new ArrayList<String>();
+        while (m.find()) {
+            strs.add(m.group(1));            
+        } 
+        for (String s : strs){
+            System.out.println(s);
+        }        
+    }
 }
