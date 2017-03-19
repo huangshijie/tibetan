db846d3593d1b423ee13774d906afc3e9557d881
Alex <Alex@192.168.0.102>
2017-03-19 13:54:05 +0800

    20170319

--- src/main/resources/static/js/pcc-select.js
+++ src/main/resources/static/js/pcc-select.js
@@ -1,129 +1,483 @@
 var data = {
-	"aaa": [
-			{"code":"baa","name": "青海省"},
-			{"code":"caa","name": "甘肃省"},
-			{"code":"daa","name": "四川省"},
-			{"code":"eaa","name": "西藏"},
-			{"code":"faa","name": "云南省"}
-		],
-		"baa": [
-			{"code":"bba", "name": "果洛藏族自治州"},
-			{"code":"bca", "name": "海西蒙古族藏族自治州"},
-			{"code":"bda", "name": "海南藏族自治州"},
-			{"code":"bea", "name": "海北藏族自治州"},
-			{"code":"bfa", "name": "黄南藏族自治州"},
-			{"code":"bga", "name": "海东市"},
-			{"code":"bha", "name": "玉树藏族自治州"}
-		],
-		"bba": [
-			{"code":"bbb", "name": " "}
-		],
-		"bca": [
-			{"code":"bcb", "name": " "}
-		],
-		"bda": [
-			{"code":"bdb", "name": " "}
-		],
-		"bea": [
-			{"code":"beb", "name": " "}
-		],
-		"bfa": [
-			{"code":"bfb", "name": " "}
-		],
-		"bga": [
-			{"code":"bgb", "name": "化隆回族自治县"},
-			{"code":"bgc", "name": "循化撒拉族自治县"},
-			{"code":"bgd", "name": "乐都县"}
-		],
-		"bha": [
-			{"code":"bhb", "name": " "}
-		],
-		"caa": [
-			{"code":"cba", "name": "武威市"},
-			{"code":"cca", "name": "甘南藏族自治州"}
-		],
-		"cba": [
-			{"code":"cbb", "name": "天祝藏族自治县"}
-		],
-		"cca": [
-			{"code":"ccb", "name": "卓尼县"},
-			{"code":"ccc", "name": "迭部县"},
-			{"code":"ccd", "name": "舟曲县"}
-		],
-		"daa": [
-			{"code":"dba", "name": "阿坝藏族羌族自治州"},
-			{"code":"dca", "name": "甘孜藏族自治州"},
-			{"code":"dda", "name": "昌都地区"}
-		],
-		"dba": [
-			{"code":"dbb", "name": " "}
-		],
-		"dca": [
-			{"code":"dcb", "name": "德格"},
-			{"code":"dcc", "name": "甘孜"},
-			{"code":"dcd", "name": "康定"},
-			{"code":"dce", "name": "雅江"},
-			{"code":"dcf", "name": "理塘"},
-			{"code":"dcg", "name": "巴塘"},
-			{"code":"dch", "name": "乡城"},
-			{"code":"dci", "name": "稻城"}
-		],
-		"dda": [
-			{"code":"ddb", "name": "昌都"},
-			{"code":"ddc", "name": "丁青县"}
-		],
-		"eaa": [
-			{"code":"eba", "name": "昌都地区"},
-			{"code":"eca", "name": "那曲地区"},
-			{"code":"eda", "name": "林芝地区东部"},
-			{"code":"eea", "name": "阿里地区"},
-			{"code":"efa", "name": "拉萨"},
-			{"code":"ega", "name": "山南地区"},
-			{"code":"eha", "name": "林芝市"},
-			{"code":"eia", "name": "日喀则市"}
-		],
-		"eba": [
-			{"code":"ebb", "name": " "}
-		],
-		"eca": [
-			{"code":"ecb", "name": "聂荣"},
-			{"code":"ecc", "name": "巴青"},
-			{"code":"ecd", "name": "索县"},
-			{"code":"ece", "name": "比如"},
-			{"code":"ecf", "name": "嘉黎"},
-			{"code":"ecg", "name": "申扎"},
-			{"code":"ech", "name": "班戈"}
-		],
-		"eda": [
-			{"code":"edb", "name": "察隅"},
-			{"code":"edc", "name": "波密"},
-			{"code":"edd", "name": "墨脱"}
-		],
-		"eea": [
-			{"code":"eeb", "name": "改则"}
-		],
-		"efa": [
-			{"code":"efb", "name": " "}
-		],
-		"ega": [
-			{"code":"egb", "name": " "}
-		],
-		"eha": [
-			{"code":"ehb", "name": "林芝市"},
-			{"code":"ehc", "name": "工布江达县"},
-			{"code":"ehd", "name": "米林县"},
-			{"code":"ehe", "name": "朗县"}
-		],
-		"eia": [
-			{"code":"eib", "name": " "}
-		],
-		"faa": [
-			{"code":"fba", "name": "迪庆藏族自治州"}
-		],
-		"fba": [
-			{"code":"fbb", "name": " "}
-		]
-	}
+		"aaa": [
+				{"code":"baa","name": "青海省"},
+				{"code":"caa","name": "甘肃省"},
+				{"code":"daa","name": "四川省"},
+				{"code":"eaa","name": "西藏自治区"},
+				{"code":"faa","name": "云南省"}
+			],
+			"baa": [
+				{"code":"bba", "name": "果洛藏族自治州"},
+				{"code":"bca", "name": "海西蒙古族藏族自治州"},
+				{"code":"bda", "name": "海南藏族自治州"},
+				{"code":"bea", "name": "海北藏族自治州"},
+				{"code":"bfa", "name": "黄南藏族自治州"},
+				{"code":"bga", "name": "海东地区"},
+				{"code":"bha", "name": "玉树藏族自治州"},
+				{"code":"bia", "name": "西宁市"}
+			],
+			"bba": [
+				{"code":"bbb", "name": "班玛县"},
+				{"code":"bbc", "name": "达日县"},
+				{"code":"bbd", "name": "甘德县"},
+				{"code":"bbe", "name": "久治县"},
+				{"code":"bbb", "name": "玛多县"},
+				{"code":"bbb", "name": "玛沁县"}
+			],
+			"bca": [
+				{"code":"bcb", "name": "都兰县"},
+				{"code":"bcc", "name": "天峻县"},
+				{"code":"bcd", "name": "乌兰县"}
+			],
+			"bda": [
+				{"code":"bdb", "name": "共和县"},
+				{"code":"bdc", "name": "贵德县"},
+				{"code":"bdd", "name": "贵南县"},
+				{"code":"bde", "name": "同德县"},
+				{"code":"bdf", "name": "兴海县"}
+			],
+			"bea": [
+				{"code":"beb", "name": "刚察县"},
+				{"code":"beb", "name": "海晏县"},
+				{"code":"beb", "name": "门源回族自治县"},
+				{"code":"beb", "name": "祁连县"}
+			],
+			"bfa": [
+				{"code":"bfb", "name": "河南蒙古族自治县"},
+				{"code":"bfb", "name": "尖扎县"},
+				{"code":"bfb", "name": "同仁县"},
+				{"code":"bfb", "name": "泽库县"}
+			],
+			"bga": [
+				{"code":"bgb", "name": "互助土族自治县"},
+				{"code":"bgc", "name": "化隆回族自治县"},
+				{"code":"bgd", "name": "乐都县"},
+				{"code":"bge", "name": "民和回族土族自治县"},
+				{"code":"bgf", "name": "平安县"},
+				{"code":"bgg", "name": "循化撒拉族自治县"},
+				{"code":"bgh", "name": "湟源县"},
+				{"code":"bgi", "name": "湟中县"}
+			],
+			"bha": [
+				{"code":"bhb", "name": "称多县"},
+				{"code":"bhc", "name": "囊谦县"},
+				{"code":"bhd", "name": "曲麻莱县"},
+				{"code":"bhe", "name": "玉树县"},
+				{"code":"bhf", "name": "杂多县"},
+				{"code":"bhg", "name": "治多县"}
+			],
+			"bia": [
+				{"code":"bib", "name": "大通回族土族自治县"}
+			],
+			"caa": [
+				{"code":"cba", "name": "武威地区"},
+				{"code":"cca", "name": "甘南藏族自治州"},
+				{"code":"cda", "name": "张掖地区"},
+				{"code":"cea", "name": "酒泉地区"}
+			],
+			"cba": [
+				{"code":"cbb", "name": "天祝藏族自治县"}
+			],
+			"cca": [
+				{"code":"ccb", "name": "迭部县"},
+				{"code":"ccc", "name": "临潭县"},
+				{"code":"ccd", "name": "碌曲县"},
+				{"code":"ccb", "name": "玛曲县"},
+				{"code":"ccc", "name": "夏河县"},
+				{"code":"ccd", "name": "舟曲县"},
+				{"code":"ccb", "name": "卓尼县"}
+			],
+			"cda": [
+				{"code":"cdb", "name": "高台县"},
+				{"code":"cdc", "name": "临泽县"},
+				{"code":"cdd", "name": "民乐县"},
+				{"code":"cde", "name": "山丹县"},
+				{"code":"cdf", "name": "肃南裕固族自治县"}
+			],
+			"cea": [
+				{"code":"ceb", "name": "阿克塞哈萨克族自治县"},
+				{"code":"cec", "name": "安西县"},
+				{"code":"ced", "name": "金塔县"},
+				{"code":"cee", "name": "肃北蒙古族自治县"}
+			],
+			"daa": [
+				{code:"dba", name: "成都市"},	
+				{code:"dca", name: "自贡市"},	
+				{code:"dda", name: "攀枝花市"},	
+				{code:"dea", name: "泸州市"},	
+				{code:"dfa", name: "德阳市"},	
+				{code:"dga", name: "绵阳市"},
+				{code:"dha", name: "广元市"},	
+				{code:"dia", name: "遂宁市"},	
+				{code:"dja", name: "内江市"},	
+				{code:"dka", name: "眉山市"},	
+				{code:"dla", name: "乐山市"},	
+				{code:"dma", name: "南充市"},	
+				{code:"dna", name: "宜宾市"},	
+				{code:"doa", name: "广安市"},	
+				{code:"dpa", name: "达州市"},
+				{code:"dqa", name: "巴中市"},	
+				{code:"dra", name: "雅安市"},	
+				{code:"dsa", name: "资阳市"},	
+				{code:"dta", name: "阿坝藏族羌族自治州"},
+				{code:"dua", name: "甘孜藏族自治州"},
+				{code:"dva", name: "凉山彝族自治州"}
+			],
+			"dba": [
+				{"code":"dbb", "name": "蒲江县"},
+				{"code":"dbc", "name": "双流县"},
+				{"code":"dbd", "name": "大邑县"},
+				{"code":"dbe", "name": "温江县"},
+				{"code":"dbf", "name": "新都县"},
+				{"code":"dbg", "name": "新津县"},
+				{"code":"dbh", "name": "郫县"},
+				{"code":"dbi", "name": "金堂县"}
+
+			],
+			"dca": [	
+				{"code":"dcb", "name": "富顺县"},
+				{"code":"dcc", "name": "荣县"}
+			],
+			"dda": [
+				{"code":"ddb", "name": "米易县"},
+				{"code":"ddc", "name": "盐边县"}
+			],
+			"dea": [
+				{"code":"deb", "name":"古蔺县"},
+				{"code":"dec", "name":"合江县"},
+				{"code":"def", "name":"叙永县"},
+				{"code":"deg", "name":"泸县"}
+			], 	
+			"dfa": [
+				{"code":"dfb", "name":"罗江县"},
+				{"code":"dfc", "name":"中江县"}
+			], 	
+			"dga": [
+				{"code":"dgb", "name":"安县"},
+				{"code":"dgc", "name":"北川县"},
+				{"code":"dgd", "name":"平武县"},
+				{"code":"dge", "name":"三台县"},
+				{"code":"dgf", "name":"盐亭县"},
+				{"code":"dgg", "name":"梓潼县"}
+			], 
+			"dha": [
+				{"code":"dhb", "name":"苍溪县"},
+				{"code":"dhc", "name":"剑阁县"},
+				{"code":"dhd", "name":"青川县"},
+				{"code":"dhe", "name":"旺苍县"}
+			], 	
+			"dia": [
+				{"code":"dib", "name":"蓬溪县"},
+				{"code":"dic", "name":"射洪县"},
+				{"code":"did", "name":"大英县"}
+			], 	
+			"dja": [	
+				{"code":"djb", "name":"隆昌县"},
+				{"code":"djc", "name":"威远县"},
+				{"code":"djd", "name":"资中县"}
+			], 	
+			"dka": [
+				{"code":"dkb", "name":"洪雅县"},
+				{"code":"dkc", "name":"仁寿县"},
+				{"code":"dkd", "name":"彭山县"},
+				{"code":"dke", "name":"丹棱县"},
+				{"code":"dkf", "name":"青神县"}
+			], 	
+			"dla": [
+				{"code":"dlb", "name":"夹江县"},
+				{"code":"dlc", "name":"井研县"},
+				{"code":"dld", "name":"沐川县"},
+				{"code":"dle", "name":"犍为县"},
+				{"code":"dlf", "name":"峨边彝族自治县"},
+				{"code":"dlg", "name":"马边彝族自治县"}
+			], 	
+			"dma": [
+				{"code":"dmb", "name":"南部县"},
+				{"code":"dmc", "name":"蓬安县"},
+				{"code":"dmd", "name":"西充县"},
+				{"code":"dme", "name":"仪陇县"},
+				{"code":"dmf", "name":"营山县"}
+			], 	
+			"dna": [
+				{"code":"dnb", "name":"长宁县"},
+				{"code":"dnc", "name":"高县"},
+				{"code":"dnd", "name":"江安县"},
+				{"code":"dne", "name":"南溪县"},
+				{"code":"dnf", "name":"屏山县"},
+				{"code":"dng", "name":"兴文县"},
+				{"code":"dnh", "name":"宜宾县"},
+				{"code":"dni", "name":"珙县"},
+				{"code":"dnj", "name":"筠连县"}
+			], 	
+			"doa": [
+				{"code":"dob", "name":"武胜县"},
+				{"code":"doc", "name":"邻水县"},
+				{"code":"dod", "name":"岳池县"}
+			], 	
+			"dpa": [
+				{"code":"dpb", "name":"达县"},
+				{"code":"dpc", "name":"大竹县"},
+				{"code":"dpd", "name":"开江县"},
+				{"code":"dpe", "name":"渠县"},
+				{"code":"dpf", "name":"宣汉县"}
+			], 
+			"dqa": [
+				{"code":"dqb", "name":"南江县"},
+				{"code":"dqc", "name":"平昌县"},
+				{"code":"dqd", "name":"通江县"}
+			], 	
+			"dra": [
+				{"code":"drb", "name":"宝兴县"},
+				{"code":"drc", "name":"汉源县"},
+				{"code":"drd", "name":"芦山县"},
+				{"code":"dre", "name":"名山县"},
+				{"code":"drf", "name":"石棉县"},
+				{"code":"drg", "name":"天全县"},
+				{"code":"drh", "name":"荥经县"}
+			], 	
+			"dsa": [
+				{"code":"dsb", "name":"乐至县"},
+				{"code":"dsc", "name":"安岳县"}
+			], 	
+			"dta": [
+				{"code":"dtb", "name":"阿坝县"},
+				{"code":"dtc", "name":"黑水县"},
+				{"code":"dtd", "name":"红原县"},
+				{"code":"dte", "name":"金川县"},
+				{"code":"dtf", "name":"理县"},
+				{"code":"dtg", "name":"马尔康县"},
+				{"code":"dth", "name":"茂县"},
+				{"code":"dti", "name":"壤塘县"},
+				{"code":"dtj", "name":"若尔盖县"},
+				{"code":"dtk", "name":"松潘县"},
+				{"code":"dtl", "name":"小金县"},
+				{"code":"dtm", "name":"汶川县"},
+				{"code":"dtn", "name":"九寨沟县"}
+			], 
+			"dua": [
+				{"code":"dub", "name":"巴塘县"},
+				{"code":"duc", "name":"白玉县"},
+				{"code":"dud", "name":"丹巴县"},
+				{"code":"due", "name":"稻城县"},
+				{"code":"duf", "name":"道孚县"},
+				{"code":"dug", "name":"德格县"},
+				{"code":"duh", "name":"甘孜县"},
+				{"code":"dui", "name":"九龙县"},
+				{"code":"duj", "name":"康定县"},
+				{"code":"duk", "name":"理塘县"},
+				{"code":"dul", "name":"炉霍县"},
+				{"code":"dum", "name":"色达县"},
+				{"code":"dun", "name":"石渠县"},
+				{"code":"duo", "name":"新龙县"},
+				{"code":"dup", "name":"雅江县"},
+				{"code":"duq", "name":"泸定县"},
+				{"code":"dur", "name":"乡城县"},
+				{"code":"dus", "name":"得荣县"}
+			], 
+			"dva": [
+				{"code":"dvb", "name":"布拖"},
+				{"code":"dvc", "name":"德昌"},
+				{"code":"dvd", "name":"甘洛"},
+				{"code":"dve", "name":"会东"},
+				{"code":"dvf", "name":"会理"},
+				{"code":"dvg", "name":"金阳"},
+				{"code":"dvh", "name":"雷波"},
+				{"code":"dvi", "name":"美姑"},
+				{"code":"dvj", "name":"冕宁"},
+				{"code":"dvk", "name":"宁南"},
+				{"code":"dvl", "name":"普格"},
+				{"code":"dvm", "name":"喜德"},
+				{"code":"dvn", "name":"盐源"},
+				{"code":"dvo", "name":"越西"},
+				{"code":"dvp", "name":"昭觉"},
+				{"code":"dvq", "name":"木里藏族自治"}
+			], 
+			"eaa": [
+				{code:"eba", name:"拉萨市"},
+				{code:"eca", name:"那曲地区"},
+				{code:"eda", name:"昌都地区"},
+				{code:"eea", name:"山南地区"},
+				{code:"efa", name:"日喀则地区"},
+				{code:"ega", name:"阿里地区"},
+				{code:"eha", name:"林芝地区"}
+			],
+			"eba": [
+				{code: "ebb", name: "当雄县"},
+				{code: "ebc", name: "堆龙德庆县"},
+				{code: "ebd", name: "林周县"},
+				{code: "ebe", name: "墨竹工卡县"},
+				{code: "ebf", name: "尼木县"},
+				{code: "ebg", name: "曲水县"},
+				{code: "ebh", name: "达孜县"}
+			],
+			"eca": [
+				{code: "ecb", name: "安多县"},
+				{code: "ecc", name: "巴青县"},
+				{code: "ecd", name: "班戈县"},
+				{code: "ece", name: "比如县"},
+				{code: "ecf", name: "嘉黎县"},
+				{code: "ecg", name: "那曲县"},
+				{code: "ech", name: "聂荣县"},
+				{code: "eci", name: "申扎县"},
+				{code: "ecj", name: "索县"},
+				{code: "eck", name: "尼玛县"}
+			],
+			"eda": [
+				{code: "edb", name:"八宿县"},
+				{code: "edc", name:"边坝县"},
+				{code: "edd", name:"察雅县"},
+				{code: "ede", name:"昌都县"},
+				{code: "edf", name:"丁青县"},
+				{code: "edg", name:"贡觉县"},
+				{code: "edh", name:"江达县"},
+				{code: "edi", name:"类乌齐县"},
+				{code: "edj", name:"洛隆县"},
+				{code: "edk", name:"芒康县"},
+				{code: "edl", name:"左贡县"},
+				{code: "edm", name:"芒康县"}
+			],
+			"eea": [
+				{code: "eeb", name:"措美县"},
+				{code: "eec", name:"错那县"},
+				{code: "eed", name:"贡嘎县"},
+				{code: "eee", name:"加查县"},
+				{code: "eef", name:"浪卡子县"},
+				{code: "eeg", name:"隆子县"},
+				{code: "eeh", name:"洛扎县"},
+				{code: "eei", name:"乃东县"},
+				{code: "eej", name:"琼结县"},
+				{code: "eek", name:"曲松县"},
+				{code: "eel", name:"桑日县"},
+				{code: "eem", name:"扎囊县"}
+			],
+			"efa": [
+				{code: "efb", name:"昂仁县"},
+				{code: "efc", name:"白朗县"},
+				{code: "efd", name:"定结县"},
+				{code: "efe", name:"定日县"},
+				{code: "eff", name:"岗巴县"},
+				{code: "efg", name:"吉隆县"},
+				{code: "efh", name:"江孜县"},
+				{code: "efi", name:"康马县"},
+				{code: "efj", name:"拉孜县"},
+				{code: "efk", name:"南木林县"},
+				{code: "efl", name:"聂拉木县"},
+				{code: "efm", name:"仁布县"},
+				{code: "efn", name:"萨嘎县"},
+				{code: "efo", name:"萨迦县"},
+				{code: "efp", name:"谢通门县"},
+				{code: "efq", name:"亚东县"},
+				{code: "efr", name:"仲巴县"}
+			],
+			"ega": [
+				{code: "egb", name:"措勤县"},
+				{code: "egc", name:"噶尔县"},
+				{code: "egd", name:"改则县"},
+				{code: "ege", name:"革吉县"},
+				{code: "egf", name:"普兰县"},
+				{code: "egg", name:"日土县"},
+				{code: "egh", name:"札达县"}
+			],
+			"eha": [
+				{code: "ehb", name:"波密县"},
+				{code: "ehc", name:"察隅县"},
+				{code: "ehd", name:"工布江达县"},
+				{code: "ehe", name:"朗县"},
+				{code: "ehf", name:"林芝县"},
+				{code: "ehg", name:"米林县"},
+				{code: "ehh", name:"墨脱县"}
+			],
+			"faa": [
+				{code:"fba", name: "昆明市"},
+				{code:"fca", name: "昭通地区"},
+				{code:"fda", name: "曲靖市"},
+				{code:"fea", name: "玉溪市"},
+				{code:"ffa", name: "思茅地区"},
+				{code:"fga", name: "临沧地区"},
+				{code:"fha", name: "保山市"},
+				{code:"fia", name: "丽江地区"},
+				{code:"fja", name: "迪庆藏族自治州"}
+			],
+			"fba": [
+				{"code":"fbb", "name": "富民县"},
+				{"code":"fbc", "name": "晋宁县"},
+				{"code":"fbd", "name": "禄劝彝族苗族自治县"},
+				{"code":"fbe", "name": "宜良县"},
+				{"code":"fbf", "name": "嵩明县"},
+				{"code":"fbg", "name": "寻甸回族彝族自治县"},
+				{"code":"fbh", "name": "呈贡县"},
+				{"code":"fbi", "name": "石林彝族自治县"}
+			],
+			"fca":[
+				{"code":"fcb", "name": "大关县"},
+				{"code":"fcc", "name": "鲁甸县"},
+				{"code":"fcd", "name": "巧家县"},
+				{"code":"fce", "name": "水富县"},
+				{"code":"fcf", "name": "绥江县"},
+				{"code":"fcg", "name": "威信县"},
+				{"code":"fch", "name": "盐津县"},
+				{"code":"fci", "name": "彝良县"},
+				{"code":"fcj", "name": "永善县"},
+				{"code":"fck", "name": "镇雄县"}
+			],
+			"fda":[
+				{"code":"fdb", "name": "富源县"},
+				{"code":"fdc", "name": "会泽县"},
+				{"code":"fdd", "name": "陆良县"},
+				{"code":"fde", "name": "罗平县"},
+				{"code":"fdf", "name": "马龙县"},
+				{"code":"fdg", "name": "师宗县"},
+				{"code":"fdh", "name": "沾益县"}
+			],
+			"fea":[
+				{"code":"feb", "name": "澄江县"},
+				{"code":"fec", "name": "峨山彝族自治县"},
+				{"code":"fed", "name": "华宁县"},
+				{"code":"fee", "name": "江川县"},
+				{"code":"fef", "name": "通海县"},
+				{"code":"feg", "name": "新平彝族傣族自治县"},
+				{"code":"feh", "name": "易门县"},
+				{"code":"fei", "name": "元江哈尼族彝族傣族自治县"}
+			],
+			"ffa":[
+				{"code":"ffb", "name": "江城哈尼族彝族自治县"},
+				{"code":"ffc", "name": "景东彝族自治县"},
+				{"code":"ffd", "name": "景谷傣族彝族自治县"},
+				{"code":"ffe", "name": "澜沧拉祜族自治县"},
+				{"code":"fff", "name": "孟连傣族拉祜族佤族自治县"},
+				{"code":"ffg", "name": "墨江哈尼族自治县"},
+				{"code":"ffh", "name": "普洱哈尼族彝族自治县"},
+				{"code":"ffi", "name": "西盟佤族自治县"},
+				{"code":"ffj", "name": "镇沅彝族哈尼族拉祜族自治县"}
+			],
+			"fga":[
+				{"code":"fgb", "name": "沧源佤族自治县"},
+				{"code":"fgc", "name": "凤庆县"},
+				{"code":"fgd", "name": "耿马傣族佤族自治县"},
+				{"code":"fge", "name": "临沧县"},
+				{"code":"fgf", "name": "双江拉祜族佤族布朗族傣族自治县"},
+				{"code":"fgg", "name": "永德县"},
+				{"code":"fgh", "name": "云县"},
+				{"code":"fgi", "name": "镇康县"}
+			],
+			"fha":[
+				{"code":"fhb", "name": "昌宁县"},
+				{"code":"fhc", "name": "龙陵县"},
+				{"code":"fhd", "name": "施甸县"},
+				{"code":"fhe", "name": "腾冲县"}
+			],
+			"fia":[
+				{"code":"fib", "name": "华坪县"},
+				{"code":"fic", "name": "丽江纳西族自治县"},
+				{"code":"fid", "name": "宁蒗彝族自治县"},
+				{"code":"fie", "name": "永胜县"}
+			],
+			"fja":[
+				{"code":"fjb", "name": "德钦县"},
+				{"code":"fjc", "name": "维西傈僳族自治县"},
+				{"code":"fjd", "name": "中甸县"}
+			]
+		};
 var provinceSelect = document.getElementById("province");
 var citySelect = document.getElementById("city");
 var countySelect = document.getElementById("county");
